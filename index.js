7dcc24520c13d8425790f6bdc0371d80147b353a
Yuriko <ygabriela.arnott@gmail.com>
2022-10-11 13:51:35 -0400

    commit tree 43336c25394c27c5eaaa78bb0ca8bbcdd410316b

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// code your solution here
+function saturdayFun(activity = 'roller-skate') {
+    return `This Saturday, I want to ${activity}!`;
+  }
+  saturdayFun();
+  function mondayWork (text = 'go to the office') {
+    return `This Monday, I will ${text}.`;
+  }
+  mondayWork ();
+
+  function wrapAdjective (text = 'go to the office') {
+    return `This Monday, I will ${text}.`;
+    wrapAdjective("%")("a dedicated programmer"); //=> "You are %a dedicated programmer%!"
+  }
+  function wrapAdjective (visualFlair ='*'){
+    const innerFunction= function(adjective = 'special'){
+      return `You are ${visualFlair}${adjective}${visualFlair}!`
+    }
+      return innerFunction
+  }
+ 
